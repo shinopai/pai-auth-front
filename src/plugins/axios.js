439d1ab509f081ab9e7aae96b6aed13fecdc49ab
shinopai <harbor.shinozaki@gmail.com>
2022-02-01 11:26:08 +0000

    alter axios.js

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 require('dotenv').config();
 
 export default axios.create({
-  baseURL: process.env.VUE_APP_NODE_ENV == 'production' ? 'https://pai-auth-api.herokuapp.com/' : 'http://localhost:3000'
+  baseURL: process.env.VUE_APP_API_ENDPOINT
 });
