e6c462bb7d0339922b351c302f0fa1e6afe2dce6
shinopai <harbor.shinozaki@gmail.com>
2022-02-01 05:57:46 +0000

    alter server.js

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const app = express();
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 const path = require('path');
-const cors = require('cors');
 const serveStatic = require('serve-static');
-app = express();
 app.use(serveStatic(__dirname + "/dist"));
-const corsOption = {
-  origin: "https://pai-auth-api.herokuapp.com",
-  credentials: true
-};
-app.use(cors(corsOption));
 const port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started ' + port);
+
+// const cors = require('cors');
+// const corsOption = {
+//   origin: "https://pai-auth-api.herokuapp.com"
+// };
+// app.use(cors(corsOption));
