eb6e9491ba445606dcb57d8ccd6cd7d714c9f89a
shinopai <harbor.shinozaki@gmail.com>
2022-02-01 07:15:06 +0000

    alter server.js

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const app = express();
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", '*');
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
 
-const cors = require('cors');
-const corsOption = {
-  origin: "https://pai-auth-api.herokuapp.com"
-};
-app.use(cors(corsOption));
+  // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+
+  app.options('*', (req, res) => {
+    // allowed XHR methods
+    res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+    res.send();
+  });
+});
+
+// const cors = require('cors');
+// const corsOption = {
+//   origin: "https://pai-auth-api.herokuapp.com"
+// };
+// app.use(cors(corsOption));
 const path = require('path');
 const serveStatic = require('serve-static');
 app.use(serveStatic(__dirname + "/dist"));
